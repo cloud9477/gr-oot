1abca9adfdd1bb26efdb2c74a86759c3419819ab
cloud9477 <cloud9477@gmail.com>
2022-08-28 22:31:18 -0400

    add pluto installation

---
+++ plutoInstall.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "analog pluto installation start for ubuntu 22.04"
+echo "install dependency"
+sudo apt-get install libxml2 libxml2-dev bison flex cmake git libaio-dev libboost-all-dev
+sudo apt-get install doxygen
+sudo apt-get install libusb-1.0-0-dev
+sudo apt-get install libavahi-common-dev libavahi-client-dev
+
+echo "install analog device driver"
+cd
+[ ! -d "./sdr/" ] && echo "sdr directory does not exist, build sdr folder."
+cd ./sdr/
+
+echo "install lib iio"
+git clone https://github.com/analogdevicesinc/libiio.git
+cd libiio
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+cd ../../
+
+echo "install lib ad9361-iio"
+git clone https://github.com/analogdevicesinc/libad9361-iio.git
+cd libad9361-iio
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+cd ../../
+
+sudo apt-get install bison flex cmake git libgmp-dev swig
+
+echo "getting pluto info"
+iio_info -s
+
+
